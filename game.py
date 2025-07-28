82793177d20ee586016736b1fd22e7d91f36df3c
Caramello404 <setapong2000@gmail.com>
2025-07-29 01:31:40 +0700

    Update game.py

--- game.py
+++ game.py
@@ -12,7 +12,6 @@ def play_game():
     print("--- Welcome to the Number Guessing Game! ---")
     print("I'm thinking of a number between 1 and 100.")
 
-    import random
     secret_number = random.randint(1, 100)
     attempts = 0
 
